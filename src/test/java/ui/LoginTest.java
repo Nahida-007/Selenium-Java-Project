75299426cb8afa0d2884bd9395dc7b2fc49bda78
Nahida Parveen <nahida.parveen00007@gmail.com>
2020-10-30 16:52:13 +0530

    Commit Login Test

---
+++ src/test/java/ui/LoginTest.java
@@ -0,0 +1,22 @@
+package ui;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginTest {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webDriver.chrome.driver", "C:\\Users\\admin\\Desktop\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.saucedemo.com/");
+		driver.findElement(By.id("user-name")).sendKeys("standard_user");
+		driver.findElement(By.id("password")).sendKeys("secret_sauce");
+		driver.findElement(By.xpath("/html/body/div[2]/div[1]/div/div/form/input[3]")).click();
+		driver.close();
+		
+
+	}
+
+}
